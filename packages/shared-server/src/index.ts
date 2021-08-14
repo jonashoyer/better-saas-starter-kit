89f6f85a8781b560f901fb05990407f412f06d14
jonashoyer <jonas.reitel@gmail.com>
2021-08-14 12:00:26 +0200

    Fixed packages linking

--- packages/shared-server/src/index.ts
+++ packages/shared-server/src/index.ts
@@ -2,6 +2,7 @@ export * from './StripeHandler';
 export * from './StripeWebhooks';
 export * from './stripe';
 export * from './QueueManager';
+export * from './queues';
 export * from './redisClient';
 export * from './pubsub';
 export * from './PubSubTriggers';
