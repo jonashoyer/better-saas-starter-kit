007ce4b7e7dc580eb9c8b12dcf43f1cb3ba76528
jonashoyer <12399677+jonashoyer@users.noreply.github.com>
2022-05-25 16:15:04 +0200

    Added stripe sync tool

---
+++ apps/cli/src/tools/stripeSync.ts
@@ -0,0 +1,53 @@
+import { createStripe, StripeHandler } from "shared-server";
+import { config } from 'dotenv';
+import path from 'path';
+import { PrismaClient } from '@prisma/client';
+
+config({
+  path: path.join(process.cwd(), '.env'),
+});
+
+const stripe = createStripe();
+const prisma = new PrismaClient();
+
+(async () => {
+  
+  const handler = new StripeHandler(stripe, prisma);
+  const products = await handler.fetchProductList();
+  const prices = await handler.fetchPriceList();
+
+  const filtedProducts = products.data.filter(e => !!e.metadata.type);
+  const filtedPrices = prices.data.filter(e => filtedProducts.some(x => x.id == e.product));
+
+  await prisma.stripeProduct.createMany({
+    data: filtedProducts.map(e => ({
+      id: e.id,
+      active: e.active,
+      metadata: e.metadata,
+      name: e.name,
+      type: e.metadata.type,
+      image: e.images?.[0] ?? null,
+
+    })),
+    skipDuplicates: true,
+  });
+
+  await prisma.stripePrice.createMany({
+    data: filtedPrices.map(e => ({
+      id: e.id,
+      productId: e.product as string,
+      active: e.active,
+      currency: e.currency,
+      type: e.type,
+      unitAmount: e.unit_amount,
+      interval: e.recurring?.interval,
+      intervalCount: e.recurring?.interval_count,
+      trialPeriodDays: e.recurring?.trial_period_days,
+      metadata: e.metadata,
+    })),
+    skipDuplicates: true,
+  });
+
+  console.log(`Imported ${filtedProducts.length} products and ${filtedPrices.length} prices`);
+  process.exit();
+})();
