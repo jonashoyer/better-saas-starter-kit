488c1af2d4b570adbb543cb02cc29990338228ce
jonashoyer <12399677+jonashoyer@users.noreply.github.com>
2022-06-02 17:23:54 +0200

    W3T Omit Statement

--- packages/web3token/src/index.ts
+++ packages/web3token/src/index.ts
@@ -8,12 +8,13 @@ export * from './utils';
 export * from './errors';
 
 
-const payloadSerializer: PayloadSerializer = (value: Web3TokenSignOptions) => {
+const payloadSerializer = (value: Web3TokenPayload, statement?: string) => {
   return [
-    { value: value.statement },
+    { value: value.statement ?? statement },
     { value: value.expiresAt && new Date(value.expiresAt).toUTCString(), name: 'Expires at' },
     { value: value.validAt && new Date(value.validAt).toUTCString(), name: 'Valid after' },
     { value: value.issuedAt && new Date(value.issuedAt as any).toUTCString(), name: 'Issued at' },
+    { value: value.address, name: 'Address' },
     { value: value.domain, name: 'Domain' },
     { value: value.w3tid, name: 'Token ID' },
     { value: value.nonce, name: 'Nonce' },
@@ -23,16 +24,18 @@ const payloadSerializer: PayloadSerializer = (value: Web3TokenSignOptions) => {
     .join('\n\n');
 }
 
-const optionsToPayload = ({ expiresIn, validIn, ...options }: Web3TokenSignOptions) => ({
+const optionsToPayload = (address: string, { expiresIn, validIn, omitStatement, ...options }: Web3TokenSignOptions, _omitStatement?: boolean) => ({
   ...options,
   expiresAt: expiresIn ? Date.now() + (typeof expiresIn == 'string' ? ms(expiresIn) : expiresIn) : options.expiresAt,
   validAt: validIn ? Date.now() + (typeof validIn == 'string' ? ms(validIn) : validIn) : options.validAt,
   nonce: options.nonce === true ? uuidv4() : (options.nonce ? options.nonce : undefined),
   issuedAt: options.issuedAt === true ? Date.now() : (options.issuedAt ? options.issuedAt : undefined),
+  statement: _omitStatement ? undefined : options.statement,
+  address,
 })
 
+export type Web3TokenPayload = { address: string } & Pick<Web3TokenSignOptions, 'expiresAt' | 'validAt' | 'issuedAt' | 'statement' | 'domain' | 'w3tid' | 'nonce'>
 
-export type PayloadSerializer<S = {}> = (value: S) => string;
 
 export interface Web3TokenSignOptions {
   expiresIn?: number | string;
@@ -43,27 +46,29 @@ export interface Web3TokenSignOptions {
 
   issuedAt?: number | boolean;
   
+  statement?: string;
   domain?: string;
   w3tid?: string;
-  statement?: string;
   nonce?: number | string | boolean;
+
+  omitStatement?: boolean;
 }
 
-const sign = (web3: Web3, account: string, options: Web3TokenSignOptions) => {
+const sign = async (web3: Web3, options: Web3TokenSignOptions) => {
 
-  const payload = optionsToPayload(options);
-
-  return signWeb3Token(web3, account, payloadSerializer(payload), payload);
+  const address = (await web3.eth.getAccounts())[0];
+  if (!address) throw new Web3TokenError('no account selected');
+  return await signWeb3Token(web3, address, payloadSerializer(optionsToPayload(address, options)), optionsToPayload(address, options, options.omitStatement));
 }
 
 
 export interface Web3TokenVerifyOptions {
   domain?: string | string[];
+  statement?: string;
 }
 
 const verify = async (web3: Web3, token: string, options?: Web3TokenVerifyOptions) => {
   const { payload, signature } = web3TokenDecode<ReturnType<typeof optionsToPayload>>(token);
-
   if (payload.domain && options?.domain) {
     
     if (Array.isArray(options.domain)) {
@@ -80,8 +85,12 @@ const verify = async (web3: Web3, token: string, options?: Web3TokenVerifyOption
     if (payload.validAt > Date.now()) throw new TokenPrematureError(payload.validAt);
   }
 
-  const message = payloadSerializer(payload);
+  const message = payloadSerializer(payload, options?.statement);
   const address = await verifyWeb3Token(web3, message, signature);
+
+  if (payload.address.toLowerCase() != address.toLowerCase()) {
+    throw new Web3TokenError('w3t malformed');
+  }
 
   return {
     address,
